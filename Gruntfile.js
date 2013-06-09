a81799957d0bf4b6decd436dadf5be61ef3a1690
Jesse Freeman <jessefreeman@gmail.com>
2013-06-08 20:20:53 -0400

    added grunt file that can run bake.php

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            dirListing: {
+                command: 'php tools/bake.php lib/impact/impact.js lib/game/main.js js/game.min.js',
+                options: {
+                    stdout: true
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+    
+    // Default task(s).
+    grunt.registerTask('default', ['shell']);
+
+};
