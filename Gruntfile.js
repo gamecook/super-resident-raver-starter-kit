f4c89a279d60eae85faa8d230b01bf8806a5e46b
Jesse Freeman <jessefreeman@gmail.com>
2013-06-08 22:35:25 -0400

    cleaned up grunt tasks to match ant build. Also removed old font textures

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,15 @@
 ﻿module.exports = function (grunt) {
 
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    
+    var entityJSON = grunt.file.read("media/textures/entities.txt");
+    var screensJSON = grunt.file.read("media/textures/screens.txt");
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -11,11 +21,99 @@
                 }
             }
         },
+        copy: {
+            tmp: {
+                files: [
+                  { src: ['index.html'], dest: 'deploy/tmp/', filter: 'isFile' },
+                  { src: ['css/**', 'js/**', 'media/**', '!**/*.txt', '!js/default.js', '!css/snap-view.css'], dest: 'deploy/tmp/' }
+                ]
+            },
+            web: {
+                files: [
+                  { expand: true, cwd: 'deploy/tmp', src: ['./**'], dest: 'deploy/web/' }
+                ]
+            },
+            blog: {
+                files: [
+                  { expand: true, cwd: 'deploy/tmp', src: ['./**'], dest: 'deploy/blog/' }
+                ]
+            },
+            wp8: {
+                files: [
+                  { expand: true, cwd: 'deploy/tmp', src: ['./**', '!**/*.ogg'], dest: 'deploy/wp8/' }
+                ]
+            }
+        },
+        clean: {
+            deploy: ["deploy"],
+            tmp: ["deploy/tmp"]
+        },
+        replace: {
+            gamePath: {
+                src: ['deploy/tmp/index.html'],             // source files array (supports minimatch)
+                dest: 'deploy/tmp/index.html',             // destination directory or file
+                replacements: [{ 
+                    from: '<script type="text/javascript" src="lib/impact/impact.js"></script>',                   // string replacement
+                    to: '' 
+                },
+                    {
+                        from: 'lib/game/main.js',                   // string replacement
+                        to: 'js/game.min.js'
+                }]
+            },
+            textureAtlas: {
+                src: ['deploy/tmp/js/game.min.js'],             // source files array (supports minimatch)
+                dest: 'deploy/tmp/js/game.min.js',             // destination directory or file
+                replacements: [{
+                    from: 'entityJSON:null,',
+                    to: 'entityJSON:'+ entityJSON+','
+                },
+                    {
+                        from: 'screenJSON:null,',
+                        to: 'screenJSON:' + screensJSON + ','
+                    }]
+            },
+            blog: {
+                src: ['deploy/blog/js/game.min.js'],
+                dest: 'deploy/blog/js/game.min.js', 
+                replacements: [{
+                    from: 'media',
+                    to: '/wp-content/games/super-resident-raver/media/'
+                }]
+            }
+        },
+        express: {
+            options: {
+                background: true
+            },
+            dev: {
+                options: {
+                    script: 'server.js'
+                }
+            }
+        },
+        watch: {
+            express: {
+                files: ['lib/**/*.js'],
+                tasks: ['express:dev'],
+                options: {
+                    nospawn: true //Without this option specified express won't be reloaded
+                }
+            }
+        }
     });
 
-    grunt.loadNpmTasks('grunt-shell');
     
     // Default task(s).
-    grunt.registerTask('default', ['shell']);
-
+    grunt.registerTask('default', ['express:dev', 'watch']);
+    grunt.registerTask('bake', ['shell',
+                                'clean:deploy',
+                                'copy:tmp',
+                                'replace:gamePath',
+                                'replace:textureAtlas',
+                                'copy:web',
+                                'copy:wp8',
+                                'copy:blog',
+                                'replace:blog',
+                                'clean:tmp']);
 };
